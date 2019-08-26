387dc30302305ff0a416511d7def9e692a1930b4
phantomvivek <phantomvivek@gmail.com>
2019-08-26 10:23:33 +0530

    Added some more TODOs in router

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const authMiddleware = require("../middlewares/auth");
-const homeController = require("../controllers/home");
-const loginController = require("../controllers/login");
+// const authMiddleware = require("../middlewares/auth");
+// const loginController = require("../controllers/login");
+//TODO: Require home controller
 
-const router = express.Router();
+//TODO: Get an instance of express router
 
 //Define routes
+//TODO: Add a route for path / for home controller
 //TODO (Additional): Add auth middleware to home controller to log session from request and successfully complete this via the browser
-router.get("/", homeController);
 
 //Login route to handle POST login calls
-router.post("/login", bodyParser.json({ limit: "100kb" }), loginController);
+//router.post("/login", bodyParser.json({ limit: "100kb" }), loginController);
 
-module.exports = router;
+//TODO: Export router
